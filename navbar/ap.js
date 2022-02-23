34907d43ae577f8d576f336164aa39bbfb3d7b10
Aleksandar2002 <kovacevicaleksandar2002@gmail.com>
2022-02-23 20:15:17 +0100

    navbar update

--- navbar/ap.js
+++ navbar/ap.js
@@ -1,8 +1,21 @@
-let menuBtn = document.querySelector("#menu-btn");
+let menuBtnDiv = document.querySelector(".menu-btn-div");
+let menuBtn = document.querySelector(".menu-btn");
 let list = document.querySelector("nav");
+let ind=0;
 
-menuBtn.addEventListener("click" , ()=>{
-    list.style.display = "block";
-    list.style.opacity = 1;
-    list.style.top = "9rem";
+menuBtnDiv.addEventListener("click" , ()=>{
+    if(ind){
+        list.style.visibility = "hidden";
+        list.style.opacity = 0;
+        list.style.left = "-20rem";
+        menuBtn.classList.remove("open");
+        ind = 0;
+    }
+    else{
+        list.style.visibility = "inherit";
+        list.style.opacity = 1;
+        list.style.left = "0rem";
+        ind= 1;
+        menuBtn.classList.add("open");
+    }
 })
